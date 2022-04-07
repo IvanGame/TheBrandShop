1cc8a9771ee3b277fa24f72fc0643c0c7ed7b708
LogoPass <ivan5456007@gmail.com>
2022-04-07 19:04:37 +0700

    Lesson-1 complete

--- #source/js/script.js
+++ #source/js/script.js
@@ -1,10 +1,19 @@
-let arrow = document.querySelectorAll('.arrow');
-for (i = 0; i < arrow.length; i++) {
-    let subMenu = arrow[i].nextElementSibling;
-    let thisArrow = arrow[i];
+// let arrow = document.querySelectorAll('.arrow');
+// for (i = 0; i < arrow.length; i++) {
+//     let subMenu = arrow[i].nextElementSibling;
+//     let thisArrow = arrow[i];
 
-    arrow[i].addEventListener('click', function () {
-        subMenu.classList.toggle('open');
-        thisArrow.classList.toggle('active');
-    });
+//     arrow[i].addEventListener('click', function () {
+//         subMenu.classList.toggle('open');
+//         thisArrow.classList.toggle('active');
+//     });
+// }
+
+let select = document.querySelector('.header__select');
+let submenu = document.querySelector('.submenu');
+let arrow = document.querySelector('.arrow');
+
+select.onclick = function () {
+    submenu.classList.toggle('open');
+    arrow.classList.toggle('active');
 }
