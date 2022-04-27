1e0f38fa1ec5ba0a88df9b366a7ffd0fc749d16a
IvanGame <ivan5456007@gmail.com>
2022-04-27 20:27:24 +0700

    lesson-5 completed

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,2 +1,6 @@
 //@prepros-append script.js
 //@prepros-append script2.js
+//@prepros-append script3.js
+//@prepros-append script4.js
+//@prepros-append script5.js
+//@prepros-append script6.js
---
+++ #source/js/script5.js
@@ -0,0 +1,8 @@
+let select3 = document.querySelector('.filter2__select-1');
+let submenu3 = document.querySelector('.filter2__submenu1');
+let arrow3 = document.querySelector('.arrow-3');
+
+select3.onclick = function () {
+    submenu3.classList.toggle('open');
+    arrow3.classList.toggle('active');
+}
